b35f4005b9bad8cd044f76414f575f753f627872
bhattin82 <bhattin@purdue.edu>
2022-09-22 14:16:32 -0400

    Login page functionality

--- app/src/main/java/com/example/pulseoximetryapplication/LoginActivity.java
+++ app/src/main/java/com/example/pulseoximetryapplication/LoginActivity.java
@@ -2,7 +2,11 @@ package com.example.pulseoximetryapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -10,5 +14,22 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_activity);
+
+        TextView enteredUsername = findViewById(R.id.username);
+        TextView enteredPassword = findViewById(R.id.password);
+        Button loginButton = findViewById(R.id.button);
+        String correctUsername = "Stacy007";
+        String correctPassword = "health";
+
+        loginButton.setOnClickListener(v -> {
+            if ((enteredUsername.getText().toString().equals(correctUsername)) && (enteredPassword.getText().toString().equals(correctPassword))) {
+                Toast.makeText(LoginActivity.this, "Successful", Toast.LENGTH_SHORT).show();
+                Intent selectMode = new Intent(LoginActivity.this, ModeSelection.class);
+                startActivity(selectMode);
+            }
+            else {
+                Toast.makeText(LoginActivity.this, "Invalid Login", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
