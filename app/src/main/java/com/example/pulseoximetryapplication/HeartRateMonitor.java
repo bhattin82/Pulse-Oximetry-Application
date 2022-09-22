877917918ec41a6391f0804e8079c1f03967fce5
bhattin82 <bhattin@purdue.edu>
2022-09-22 10:50:59 -0400

    Heart Rate Page

---
+++ app/src/main/java/com/example/pulseoximetryapplication/HeartRateMonitor.java
@@ -0,0 +1,14 @@
+package com.example.pulseoximetryapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class HeartRateMonitor extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.heart_rate_monitor);
+    }
+}
