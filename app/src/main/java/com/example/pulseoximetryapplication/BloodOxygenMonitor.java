85164a9f608b8864d72b03e237d71ad8f15a6772
bhattin82 <bhattin@purdue.edu>
2022-09-22 12:28:12 -0400

    Blood Oxygen Page

---
+++ app/src/main/java/com/example/pulseoximetryapplication/BloodOxygenMonitor.java
@@ -0,0 +1,14 @@
+package com.example.pulseoximetryapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class BloodOxygenMonitor extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.blood_oxygen_monitor);
+    }
+}
